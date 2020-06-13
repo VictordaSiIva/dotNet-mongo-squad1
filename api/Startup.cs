4586f6ba39c2e88d9e68df72c264ca616132b545
Victor da Silva <Victor10019@hotmail.com>
2020-06-12 21:05:36 -0300

    add deploy

--- api/Startup.cs
+++ api/Startup.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-
+using Microsoft.OpenApi.Models;
 
 namespace api
 {
@@ -27,6 +26,11 @@ namespace api
 
             services.AddSingleton<Api.Data.MongoDB>();
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TeacherADM Api", Version = "v1" });
+            });
         }
 
        
@@ -45,6 +49,12 @@ namespace api
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TeacherADM API ");
+            });
         }
 
         public CorsPolicy GenerateCorsPolicy()
@@ -52,9 +62,7 @@ namespace api
             var corsBuilder = new CorsPolicyBuilder();
             corsBuilder.AllowAnyHeader();
             corsBuilder.AllowAnyMethod();
-            corsBuilder.AllowAnyOrigin(); // For anyone access.
-                                          //corsBuilder.WithOrigins("http://localhost:56573"); // for a specific url. Don't add a forward slash on the end!
-
+            corsBuilder.AllowAnyOrigin();
             return corsBuilder.Build();
         }
     }
